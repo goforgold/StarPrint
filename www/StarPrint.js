c238dec813ebdb28dc89e40cbc996cc90a576785
Shashwat <shashwat2691@gmail.com>
2014-05-19 13:45:26 +0530

    ccc

--- www/StarPrint.js
+++ www/StarPrint.js
@@ -3,34 +3,24 @@
  */
 var StarPrint = function(){};
 
-PrintPlugin.prototype.print = function(msg, successCallback, errorCallback) {
+StarPrint.prototype.print = function(msg, successCallback, errorCallback) {
 	
 	cordova.exec(successCallback, errorCallback, 'StarPrint', 'print', [msg]);
 	
 };
 
-PrintPlugin.prototype.isPrintingAvailable = function(successCallback, errorCallback){
+StarPrint.prototype.isPrintingAvailable = function(successCallback, errorCallback){
 	cordova.exec(successCallback, errorCallback, 'StarPrint', 'isPrintingAvailable', []);
 };
 
 // Plug in to Cordova
-//cordova.addConstructor(function() {
-//
-//    if (!window.Cordova) {
-//        window.Cordova = cordova;
-//    };
-//
-//
-//    if(!window.plugins) window.plugins = {};
-//    window.plugins.StarPrint = new StarPrint();
-//});
-try{
-	
+cordova.addConstructor(function() {
 
-if(!window.plugins) window.plugins = {};
-    window.StarPrint = new StarPrint();
-}
-catch(ex)
-{
-	alert(ex);
-}
+    if (!window.Cordova) {
+        window.Cordova = cordova;
+    };
+
+
+    if(!window.plugins) window.plugins = {};
+    window.plugins.StarPrint = new StarPrint();
+});
