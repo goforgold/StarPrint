28a20cb059639aa8101d4978ba8aca49a470b498
Shashwat <shashwat2691@gmail.com>
2014-05-22 11:40:30 +0530

    ccc

--- src/ios/StarPrint.m
+++ src/ios/StarPrint.m
@@ -46,62 +46,91 @@
     
     CDVPluginResult* pluginResult = nil;
     
-    //unsigned char printCommand[] = {0x41, 0x42, 0x43, 0x44, 0x1B, 0x7A, 0x00, 0x1B, 0x64, 0x02};
-    unsigned char* printCommand = (unsigned char*) [self.printContent UTF8String];
-    uint bytesWritten = 0;
-    StarPrinterStatus_2 starPrinterStatus;
-    SMPort *port = nil;
+    // call sendCommand here
+    
+    NSString *printerReponse = [self sendCommand:[self.printContent dataUsingEncoding:NSUTF8StringEncoding] portName:self.portName portSettings:@"Standard" timeoutMillis:10000];
+    
+    if (printerReponse == nil || [printerReponse isEqual: @""]) {
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"{\"success\": true}"];
+    }
+    else {
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[NSString stringWithFormat:@"{\"success\": false, \"available\": true, \"error\": \"%@\"}", printerReponse]];
+    }
+    
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (NSString *)sendCommand:(NSData *)commandsToPrint portName:(NSString *)_portName portSettings:(NSString *)_portSettings timeoutMillis:(u_int32_t)timeoutMillis
+{
+    int commandSize = (int)[commandsToPrint length];
+    unsigned char *dataToSentToPrinter = (unsigned char *)malloc(commandSize);
+    [commandsToPrint getBytes:dataToSentToPrinter];
+    
+    SMPort *starPort = nil;
+    
+    NSString *returnMsg = @"";
+    
     @try
     {
-        port = [SMPort getPort:self.portName :@"" :10000];
-        //Start checking the completion of printing
-        [port beginCheckedBlock:&starPrinterStatus :2];
-        if (starPrinterStatus.offline == SM_TRUE)
+        starPort = [SMPort getPort:_portName :_portSettings :timeoutMillis];
+        if (starPort == nil)
         {
-            //There was an error writing to the port
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[NSString stringWithFormat:@"{success: false, available: true, error: \"%@\"}", @"Printer is in Offline mode."]];
+            returnMsg = @"Failed to Open Port";
+            return returnMsg;
+        }
+        
+        StarPrinterStatus_2 status;
+        [starPort beginCheckedBlock:&status :2];
+        if (status.offline == SM_TRUE) {
+            returnMsg = @"Printer is offline";
+            return returnMsg;
         }
         
         struct timeval endTime;
         gettimeofday(&endTime, NULL);
-        endTime.tv_sec += 10;
-        BOOL timedOut = NO;
+        endTime.tv_sec += 30;
         
-        while (bytesWritten < sizeof (printCommand)) {
-            bytesWritten += [port writePort: printCommand : bytesWritten : sizeof (printCommand) - bytesWritten];
+        int totalAmountWritten = 0;
+        while (totalAmountWritten < commandSize)
+        {
+            int remaining = commandSize - totalAmountWritten;
+            int amountWritten = [starPort writePort:dataToSentToPrinter :totalAmountWritten :remaining];
+            totalAmountWritten += amountWritten;
             
             struct timeval now;
             gettimeofday(&now, NULL);
             if (now.tv_sec > endTime.tv_sec)
             {
-                timedOut = YES;
                 break;
             }
         }
         
-        //End checking the completion of printing
-        [port endCheckedBlock:&starPrinterStatus :2];
+        if (totalAmountWritten < commandSize)
+        {
+            returnMsg = @"Write port timed out";
+            
+        }
         
-        if (timedOut) {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[NSString stringWithFormat:@"{\"success\": false, \"available\": true, \"error\": \"%@\"}", @"Could not send data to printer in 10 seconds."]];
-        }
-        else if (starPrinterStatus.offline == SM_TRUE) {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[NSString stringWithFormat:@"{\"success\": false, \"available\": true, \"error\": \"%@\"}", @"Printer got in Offline mode while sending data."]];
-        }
-        else {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"{\"success\": true}"];
+        starPort.endCheckedBlockTimeoutMillis = 30000;
+        [starPort endCheckedBlock:&status :2];
+        if (status.offline == SM_TRUE) {
+            returnMsg = @"Printer is offline";
+            return returnMsg;
         }
     }
-    @catch (PortException *ex)
+    @catch (PortException *exception)
     {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[NSString stringWithFormat:@"{\"success\": false, \"available\": true, \"error\": \"%@\"}", ex.reason]];
+        returnMsg = @"Write port timed out";
     }
     @finally
     {
-        [SMPort releasePort:port];
+        free(dataToSentToPrinter);
+        [SMPort releasePort:starPort];
+        return returnMsg;
     }
-    
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 
